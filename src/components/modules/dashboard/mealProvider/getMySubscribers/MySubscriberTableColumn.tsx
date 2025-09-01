2491c8cc221de7698292d922f6eb23929021f98b
theabsparrow <absbashar04@gmail.com>
2025-09-02 02:24:37 +0600

    meal provider loader added done

--- src/components/modules/dashboard/mealProvider/getMySubscribers/MySubscriberTableColumn.tsx
+++ src/components/modules/dashboard/mealProvider/getMySubscribers/MySubscriberTableColumn.tsx
@@ -1,8 +1,12 @@
 "use client";
+import ConfirmDelation from "@/components/confirmDeletion/ConfirmDeletion";
+import { deleteBlog } from "@/services/blogService";
 import { TGetAllSubscribersType } from "@/types/kitchenSubscriberTypes";
 import { convertDate } from "@/utills/dateConverter";
 import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
+import { toast } from "sonner";
 
 export const mySubscribertableColumn =
   (): ColumnDef<TGetAllSubscribersType>[] => [
@@ -10,16 +14,8 @@ export const mySubscribertableColumn =
       id: "name",
       header: "Subscriber`s Name",
       cell: ({ row }) => {
-        const name = row.original?.subscriberInfo?.name;
-        const id = row.original?.subscriberInfo._id;
-        return (
-          <Link
-            href={`/mealProvider/customer/${id}`}
-            className="text-primary hover:underline duration-500"
-          >
-            {name}
-          </Link>
-        );
+        const name = row?.original?.subscriberInfo?.name;
+        return <p>{name}</p>;
       },
     },
     {
@@ -39,6 +35,63 @@ export const mySubscribertableColumn =
           <p>
             {date?.creationDate} {date?.creationTime}
           </p>
+        );
+      },
+    },
+    {
+      id: "details",
+      header: "View",
+      cell: ({ row }) => {
+        const id = row?.original?.subscriberInfo?._id;
+        return (
+          <Link
+            href={`/mealProvider/customer/${id}`}
+            className="text-sm bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700 transition"
+          >
+            View Subscriber`s Info
+          </Link>
+        );
+      },
+    },
+    {
+      id: "delete",
+      header: "Action",
+      cell: ({ row }) => {
+        const id = row?.original?._id;
+        const name = row.original?.subscriberInfo?.name;
+
+        const handleDelete = async (
+          setLoading: Dispatch<SetStateAction<boolean>>,
+          setOpen: Dispatch<SetStateAction<boolean>>
+        ) => {
+          setLoading(true);
+          if (!id) {
+            toast.error("falid to remove blog", { duration: 3000 });
+            setLoading(false);
+            return;
+          }
+          const toastId = toast.loading("Removing blog...");
+          try {
+            const result = await deleteBlog(id);
+            if (result?.success) {
+              toast.success(result?.message, { id: toastId, duration: 3000 });
+              setOpen(false);
+              setLoading(false);
+            } else {
+              toast.error(result?.message, { id: toastId, duration: 3000 });
+              setLoading(false);
+            }
+          } catch (error: any) {
+            console.log(error);
+          }
+          setLoading(true);
+        };
+
+        return (
+          <ConfirmDelation
+            value={`${name}'s subscription`}
+            handleDelete={handleDelete}
+          />
         );
       },
     },
